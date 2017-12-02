c8c32c4c797fe1a228857396494b366fdcb13a41
Jude Hordern <judemitchell36@gmail.com>
2017-12-02 14:12:22 +1100

    extract stop names even if there are newlines in a stop name

--- html_extract.py
+++ html_extract.py
@@ -1,15 +1,15 @@
 from bs4 import BeautifulSoup
 import re
 
-print '***************************'
-print '******** TEST AREA ********'
-print '***************************'
+# print '***************************'
+# print '******** TEST AREA ********'
+# print '***************************'
 
 
 
-print '***************************'
-print '***************************'
-print '***************************'
+# print '***************************'
+# print '***************************'
+# print '***************************'
 
 stop1 = '1-University of Melbourne/Swanston St (Carlton)'
 stop2 = '14-Arts Centre/St Kilda Rd (Southbank)'
@@ -17,23 +17,24 @@ html_file = "./HTMLS/2010Route1.html"
 
 content = open(html_file, 'r')
 soup = BeautifulSoup(content.read(), "html.parser")
-
 tags = soup.find_all(["br", "span"])
 
 for x in range(len(tags)):
-	contains_stops = re.search(r'\d{1,3}-[A-Z][A-z /]+\([A-Z][A-z ]+\)', str(tags[x]))
+	contains_stops = re.search(r'\d{1,3}-[A-Z][A-z\s\n/]+\([A-Z][A-z\s\n]+\)', str(tags[x]))
 	if contains_stops:
 		stops_tag_num = x
 		break
 
-stops = re.findall(r'\d{1,3}-[A-Z][A-z /]+\([A-Z][A-z ]+\)', str(tags[stops_tag_num]))
+stops_tag = str(tags[stops_tag_num])
+stops_tag = stops_tag.replace('<br>', '')
+stops_tag = stops_tag.replace('\n', ' ')
 
-number_stops = len(stops)
+number_stops = len(re.findall(r'\d{1,3}-[A-Z]', stops_tag))
+stop_names = re.findall(r'\d{1,3}-[A-Z][A-z\s\n/]+\([A-Z][A-z\s\n]+\)', stops_tag)
 
-stop1_num = stops.index(stop1)
+# extract the stops
 
-print stop1_num
+# stop1_num = stops.index(stop1)
+# stop2_num = stops.index(stop2)
 
-# determine the number of rows in each little table
-
-# determine the row values for each stop 
+# do I need to know that there was that dodgy stop in there - count the number of lines in the stops tag?
