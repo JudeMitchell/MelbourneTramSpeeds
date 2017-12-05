38aeecc9ee7949327106f91adfd6ac6806cc253b
Jude Hordern <judemitchell36@gmail.com>
2017-12-05 15:27:19 +1100

    now extracts times to lists

--- html_extract.py
+++ html_extract.py
@@ -1,28 +1,39 @@
 from bs4 import BeautifulSoup
 import re
+# from sys import argv
+from my_functions import *
 
-# print '***************************'
-# print '******** TEST AREA ********'
-# print '***************************'
-
-
-
-# print '***************************'
-# print '***************************'
-# print '***************************'
-
+# todo - read these in from a csv
 stop1 = '1-University of Melbourne/Swanston St (Carlton)'
 stop2 = '14-Arts Centre/St Kilda Rd (Southbank)'
 html_file = "./HTMLS/2010Route1.html"
 
+time_regex = r'\d{1,2}:\d{2}'
+stop_name_regex = r'\d{1,3}-[A-Z][A-z\s\n/]+\([A-Z][A-z\s\n]+\)'
+
 content = open(html_file, 'r')
 soup = BeautifulSoup(content.read(), "html.parser")
 tags = soup.find_all(["br", "span"])
+span_tags = soup.find_all("span")
 
-for x in range(len(tags)):
-	contains_stops = re.search(r'\d{1,3}-[A-Z][A-z\s\n/]+\([A-Z][A-z\s\n]+\)', str(tags[x]))
+## DIRECTIONS
+# todo - scrape these out
+direction1 = "East Coburg to South Melbourne"
+direction2 = "South Melbourne to East Coburg"
+
+# possible_directions = []
+
+# for i in range(len(span_tags)):
+# 	if re.search(r"[A-z ]+ to [A-z ]+", str(span_tags[i])):
+# 		print '\n\t\t***' + str(i)
+# 		print span_tags[i].contents[0]
+
+
+
+for i in range(len(tags)):
+	contains_stops = re.search(stop_name_regex, str(tags[i]))
 	if contains_stops:
-		stops_tag_num = x
+		stops_tag_num = i
 		break
 
 stops_tag = str(tags[stops_tag_num])
@@ -30,9 +41,90 @@ stops_tag = stops_tag.replace('<br>', '')
 stops_tag = stops_tag.replace('\n', ' ')
 
 number_stops = len(re.findall(r'\d{1,3}-[A-Z]', stops_tag))
-stop_names = re.findall(r'\d{1,3}-[A-Z][A-z\s\n/]+\([A-Z][A-z\s\n]+\)', stops_tag)
+stop_names = re.findall(stop_name_regex, stops_tag)
 
-# extract the stops
+
+# DETERMINE THE NUMBER OF COLUMNS OF TIMES
+num_time_cols = 0
+
+for i in range(stops_tag_num):
+	if re.search('>am', str(tags[i])):
+		num_time_cols += 1
+
+
+
+## DIRECTION ONE
+
+for i in range(len(span_tags)):
+	if re.search(direction2, str(span_tags[i])):
+		direction2_start_tag_num = i
+		break
+
+
+
+## GET INCOMPLETE TOP ROWS
+tags_with_incomplete_rows = []
+
+for i in range(stops_tag_num):
+	if re.search(time_regex, str(tags[i])):
+		tags_with_incomplete_rows.append(re.findall(time_regex, str(tags[i])))
+
+incomplete_rows_as_cols = take_longest_lists(tags_with_incomplete_rows, True)
+
+num_cols_blank_in_top_rows = num_time_cols - len(incomplete_rows_as_cols)
+
+blank_cols_in_top_rows = []
+
+for i in range (num_cols_blank_in_top_rows):
+	blank_cols_in_top_rows.append(['-','-'])
+
+incomplete_rows_as_cols = blank_cols_in_top_rows + incomplete_rows_as_cols
+
+## GET BULK OF TIMETABLE DATA
+tags_with_complete_rows= []
+
+for i in range(stops_tag_num, direction2_start_tag_num - 1):
+	if re.search(time_regex, str(tags[i])):
+		tags_with_complete_rows.append(re.findall(time_regex, str(tags[i])))
+
+cols_from_full_rows = take_longest_lists(tags_with_complete_rows, False)
+
+
+
+print '***************************'
+print '******** TEST AREA ********'
+print '***************************'
+
+
+
+print '***************************'
+print '***************************'
+print '***************************'
+
+
+
+
+
+# text_file = open('direction1_dump.txt', 'w')
+
+# for i in range(direction2_start_tag_num):
+# 	if re.search(r'\d{1,2}:\d{2}', str(tags[i])):
+# 		text_file.write('\n\t\t***' + str(i))
+# 		text_file.write(str(tags[i]))
+
+# text_file.close()
+
+
+
+
+
+
+# make sense of the times
+# good freaking luck
+
+
+
+
 
 # stop1_num = stops.index(stop1)
 # stop2_num = stops.index(stop2)
---
+++ my_functions.py
@@ -0,0 +1,24 @@
+def take_longest_lists(raw_lists, top_rows):
+
+	print '**** INSIDE ****'
+
+	longest_list_len = len(raw_lists[0])
+	if top_rows:
+		longest_list_len += 1
+
+	number_raw_lists = len(raw_lists)
+
+	clean_lists = []
+	previous_length = 0
+
+	for i in range(30):
+		if top_rows:
+			if i % longest_list_len == 1:
+				clean_lists.append(raw_lists[i])
+		else:
+			if len(raw_lists[i]) > previous_length:
+				clean_lists.append(raw_lists[i])
+				
+			previous_length = len(raw_lists[i])
+
+	return clean_lists
